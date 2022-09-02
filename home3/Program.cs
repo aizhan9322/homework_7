0ca09a8c3e821cfc71a85392b3faa485ad76702c
aizhan9322 <110331545+aizhan9322@users.noreply.github.com>
2022-09-02 21:31:35 +0300

    add home 3

---
+++ home3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+
+
+Console.WriteLine("n:");
+        int n=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("m:");
+        int m = Convert.ToInt32(Console.ReadLine());
+        int [,]arr=new int [n, m];
+        Random ran = new Random();
+        int sum = 0;
+        float middle = 0;
+
+        for (int i=0; i < n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                arr[i, j] = ran.Next(0, 100);
+
+
+                    sum += arr[i, j];
+
+                 middle = sum /( m*n);
+
+                Console.Write(arr[i, j] + "\t");
+
+
+            }
+            Console.WriteLine();
+
+        }
+        Console.WriteLine("middle=" + middle);
+
+        Console.ReadLine();
